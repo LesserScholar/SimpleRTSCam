8134ce9b3881d59ff0c711b55c45a123b6b31052
Lesser Scholar <lesserscholar@gmail.com>
2022-03-13 18:14:12 +0200

    Add formation selection icons

--- SubModule.cs
+++ SubModule.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using SandBox.ViewModelCollection;
+using System;
+using System.Collections.Generic;
 using TaleWorlds.Core;
+using TaleWorlds.Engine.GauntletUI;
 using TaleWorlds.Engine.Screens;
+using TaleWorlds.GauntletUI.Data;
 using TaleWorlds.InputSystem;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.GauntletUI;
 using TaleWorlds.MountAndBlade.View.Missions;
+using TaleWorlds.MountAndBlade.ViewModelCollection.OrderOfBattle;
 
 namespace SimpleRTSCam
 {
@@ -12,6 +18,12 @@ namespace SimpleRTSCam
     {
         bool _battleStarted = false;
         bool _inRtsCam = false;
+
+        private OrderOfBattleVM? _dataSource;
+        private GauntletLayer? _gauntletLayer;
+        private IGauntletMovie? _movie;
+        private MissionOrderGauntletUIHandler? _orderUIHandler;
+
         public override void OnMissionScreenInitialize()
         {
             base.OnMissionScreenInitialize();
@@ -21,11 +33,48 @@ namespace SimpleRTSCam
                 deploymentView.OnDeploymentFinish = (OnPlayerDeploymentFinishDelegate)Delegate.Combine(deploymentView.OnDeploymentFinish,
                     new OnPlayerDeploymentFinishDelegate(OnDeploymentFinish));
             }
+
+            _dataSource = new OrderOfBattleVM();
+
+            _gauntletLayer = new GauntletLayer(this.ViewOrderPriority, "GauntletLayer", false);
+            _movie = _gauntletLayer.LoadMovie("RTSFormations", _dataSource);
+            _orderUIHandler = base.Mission.GetMissionBehavior<MissionOrderGauntletUIHandler>();
+            MissionScreen.AddLayer(_gauntletLayer);
+        }
+        public override void OnMissionScreenFinalize()
+        {
+            if (_gauntletLayer != null)
+            {
+                _gauntletLayer.ReleaseMovie(this._movie);
+                MissionScreen.RemoveLayer(this._gauntletLayer);
+            }
+            base.OnMissionScreenFinalize();
         }
         void OnDeploymentFinish()
         {
             _battleStarted = true;
+            this._dataSource?.Initialize(base.Mission, base.MissionScreen.CombatCamera, 
+                new Action<int>(this.SelectFormationAtIndex), new Action<int>(this.DeselectFormationAtIndex), 
+                new Action(this.NoOp), new Action(this.NoOp), 
+                new Dictionary<int, Agent>(), new Action<Agent>(this.FocusOnAgent));
         }
+        private void NoOp() { }
+        private void SelectFormationAtIndex(int index)
+        {
+            if (_orderUIHandler != null)
+                _orderUIHandler.SelectFormationAtIndex(index);
+        }
+
+        private void DeselectFormationAtIndex(int index)
+        {
+            if (_orderUIHandler != null)
+                _orderUIHandler.DeselectFormationAtIndex(index);
+        }
+        private void FocusOnAgent(Agent agent)
+        {
+            MissionScreen.SetAgentToFollow(agent);
+        }
+
         public override void OnMissionScreenTick(float dt)
         {
             base.OnMissionScreenTick(dt);
@@ -34,12 +83,13 @@ namespace SimpleRTSCam
             {
                 if (base.MissionScreen.SceneLayer.Input.IsKeyPressed(InputKey.RightMouseButton))
                 {
-                    ScreenManager.FirstHitLayer.InputRestrictions.SetMouseVisibility(false);
+                    _gauntletLayer?.InputRestrictions.SetMouseVisibility(false);
                 }
                 if (base.MissionScreen.SceneLayer.Input.IsKeyReleased(InputKey.RightMouseButton))
                 {
-                    ScreenManager.FirstHitLayer.InputRestrictions.SetMouseVisibility(true);
+                    _gauntletLayer?.InputRestrictions.SetMouseVisibility(true);
                 }
+                _dataSource?.Tick();
             }
             if (_battleStarted && Input.IsKeyPressed(InputKey.F10))
             {
@@ -47,14 +97,21 @@ namespace SimpleRTSCam
                 {
                     Mission.SetMissionMode(MissionMode.Deployment, false);
                     Mission.ClearDeploymentPlanForSide(Mission.PlayerTeam.Side);
-                    ScreenManager.FirstHitLayer.InputRestrictions.SetMouseVisibility(true);
+                    _gauntletLayer?.InputRestrictions.SetMouseVisibility(true);
+                    _gauntletLayer?.InputRestrictions.SetInputRestrictions(true, InputUsageMask.All);
+
                     _inRtsCam = true;
+                    if (_dataSource != null)
+                        _dataSource.IsEnabled = true;
                 }
                 else if (Mission.Mode == MissionMode.Deployment)
                 {
                     Mission.SetMissionMode(MissionMode.Battle, false);
-                    ScreenManager.FirstHitLayer.InputRestrictions.SetMouseVisibility(false);
+                    _gauntletLayer?.InputRestrictions.SetMouseVisibility(false);
+                    _gauntletLayer?.InputRestrictions.ResetInputRestrictions();
                     _inRtsCam = false;
+                    if (_dataSource != null)
+                        _dataSource.IsEnabled = false;
                 }
             }
         }
